1e3a8b1a600769c7b7a20c72bad7796f60a343ff
Meghan V. O'Neill <u0759157@utah.edu>
2020-09-25 22:45:55 -0600

    Final implementation with faster run time. Memoization occurs before loop or recursion.

--- PA4/Under_the_Rainbow.py
+++ PA4/Under_the_Rainbow.py
@@ -11,7 +11,6 @@ def main():
         dist.append(int(input()))
 
     min_penalty = penalty(dist)
-
     print(int(min_penalty))
 
     return
@@ -26,27 +25,18 @@ def penalty(dist, i=0, penalties={}):
     if k == n:
         return 0
 
+    if i in penalties.keys():
+        return penalties[i]
+
     min_p = math.inf
 
     for k in range(i + 1, n):
-        # Use memoization.
-        if (i, k) in penalties.keys():
-            p_of_i = penalties[(i, k)]
-            min_p = min(p_of_i, min_p)
-        # Otherwise, we haven't seen this path yet.
-        else:
-            p_of_i = 400 - (dist[k] - dist[i])
-            penalties[(i, k)] = p_of_i * p_of_i + penalty(dist, k, penalties)
-            min_p = min(penalties[(i, k)], min_p)
+        p_of_i = 400 - (dist[k] - dist[i])
+        calculated_penalty = p_of_i * p_of_i + penalty(dist, k, penalties)
+        min_p = min(calculated_penalty, min_p)
 
+    penalties[i] = min_p
     return min_p
-
-
-def distance(a, b):
-
-    dist = b - a    # dist = abs(b - a)
-
-    return dist
 
 
 if __name__ == '__main__':
